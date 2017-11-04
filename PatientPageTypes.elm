3f4f5a12334f53081955820721b46af51c08eecf
Sharbi <seanharbison001@gmail.com>
2017-11-04 10:16:51 +0000

    Created Values for Model of PatientView

---
+++ PatientPageTypes.elm
@@ -0,0 +1,32 @@
+type Doctype
+    = GP
+    | Inpatient
+    | Letter
+
+type Importance
+    = High
+    | Intermediate
+    | Low
+
+type alias Patient = {
+  id : Int
+, name : String
+, age : String
+, dob : String
+, entries : List Entries
+, medications : List Drug
+}
+
+type alias Entries = {
+  title: String
+, text: String
+, docType: Doctype
+, importance: Importance
+}
+
+type alias Drug = {
+  name: String
+, dose: String
+, frequency: String
+, repeat: Bool
+}
