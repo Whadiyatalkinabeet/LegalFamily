37c07f9d86e124ccb04273d3d7f2d130f801dba1
Whadiyatalkinabeet <NyalSadiq@hotmail.com>
2017-11-04 20:37:23 +0000

    Card test

--- PatientView.elm
+++ PatientView.elm
@@ -5,6 +5,9 @@ import Html exposing (Html, text, h1, div, span, a)
 import Material
 import Material.Scheme as Scheme
 import Material.Button as Button
+import Material.Card as Card
+import Material.Color as Color
+import Material.Options as Options
 import Material.Options exposing (cs)  -- NB Avoiding inline css; use cs to select community.css classes
 import Material.List as Lists
 import Material.Table as Table
@@ -19,17 +22,22 @@ emptyPatient = Patient 4000 "" "" "" [] []
 emptyEntry : Entry
 emptyEntry = Entry 0 "" "" PatientPageTypes.GP PatientPageTypes.Low False
 
+white : Options.Property c m
+white =
+  Color.text Color.white
+
+
 patientView : Model -> Int -> Html Msg
 patientView model id =
   let patient = (Maybe.withDefault emptyPatient) (D.get id model.patients)
       entries = patient.entries
-  in div [] [ text patient.name, text "\n", entryIterator entries ]
-
+  in div [] [ text patient.name, text "\n", entryIterator entries]
 
 entryIterator : List Entry -> Html Msg
 entryIterator entries = 
     case entries of
       [] -> div [] []
       (x::xs) -> let entry = x
-                 in div [] [ text entry.title, entryIterator xs]
+                 in div [] [ Card.view [] [Card.title [] [ Card.head [ white ] [ text entry.title ] ] ]
+                            , entryIterator xs]
 
