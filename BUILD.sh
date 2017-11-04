5e35ee18d52602f41d7f90093a6885615c284cb7
Tim Day <timday@bottlenose.demon.co.uk>
2017-11-04 22:10:02 +0000

    Log out something about serving on all interfaces

--- BUILD.sh
+++ BUILD.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-(cd tools ; nodejs fakedata.js) && elm-make Main.elm --output=main.js && elm-reactor
+(cd tools ; nodejs fakedata.js) && elm-make Main.elm --output=main.js && echo "Running elm-reactor for all interfaces..." && elm-reactor -a 0.0.0.0
