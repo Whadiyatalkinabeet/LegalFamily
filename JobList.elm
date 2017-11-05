3931e93bea040d41f6f43a0568e2faffd72c0945
Tim Day <timday@bottlenose.demon.co.uk>
2017-11-05 10:19:47 +0000

    Use "real" titles

--- JobList.elm
+++ JobList.elm
@@ -8,53 +8,53 @@ import Job exposing (..)
 
 jobList : Dict Int Job
 jobList = D.fromList [
-  (0 , Job 0 0 "Blood test" False )
+  (0 , Job 0 25 "Referral" False )
   ,
-  (1 , Job 1 3 "CT scan" True )
+  (1 , Job 1 26 "Cannula" False )
   ,
-  (2 , Job 2 27 "CT scan" False )
+  (2 , Job 2 21 "CT scan" False )
   ,
-  (3 , Job 3 27 "Referral" False )
+  (3 , Job 3 3 "Biopsy" False )
   ,
-  (4 , Job 4 21 "Biopsy" False )
+  (4 , Job 4 7 "CT scan" False )
   ,
-  (5 , Job 5 2 "Biopsy" False )
+  (5 , Job 5 7 "MRI scan" False )
   ,
-  (6 , Job 6 25 "Ultrasound" False )
+  (6 , Job 6 11 "Cannula" True )
   ,
-  (7 , Job 7 6 "Biopsy" False )
+  (7 , Job 7 4 "Ultrasound" False )
   ,
-  (8 , Job 8 20 "Referral" True )
+  (8 , Job 8 13 "Cannula" True )
   ,
-  (9 , Job 9 15 "Biopsy" False )
+  (9 , Job 9 20 "Cannula" False )
   ,
-  (10 , Job 10 10 "Cannula" True )
+  (10 , Job 10 25 "Biopsy" False )
   ,
-  (11 , Job 11 29 "Cannula" False )
+  (11 , Job 11 19 "Biopsy" False )
   ,
-  (12 , Job 12 6 "Ultrasound" False )
+  (12 , Job 12 11 "MRI scan" True )
   ,
-  (13 , Job 13 23 "Cannula" False )
+  (13 , Job 13 26 "Biopsy" True )
   ,
-  (14 , Job 14 19 "MRI scan" False )
+  (14 , Job 14 6 "CT scan" False )
   ,
-  (15 , Job 15 29 "Biopsy" False )
+  (15 , Job 15 18 "Biopsy" True )
   ,
-  (16 , Job 16 11 "CT scan" False )
+  (16 , Job 16 23 "MRI scan" False )
   ,
-  (17 , Job 17 29 "Blood test" False )
+  (17 , Job 17 15 "Biopsy" False )
   ,
-  (18 , Job 18 18 "Blood test" True )
+  (18 , Job 18 13 "Referral" False )
   ,
-  (19 , Job 19 8 "Cannula" True )
+  (19 , Job 19 21 "Blood test" False )
   ,
-  (20 , Job 20 2 "Ultrasound" True )
+  (20 , Job 20 27 "Blood test" True )
   ,
-  (21 , Job 21 20 "Cannula" False )
+  (21 , Job 21 26 "CT scan" False )
   ,
-  (22 , Job 22 28 "Blood test" False )
+  (22 , Job 22 8 "Referral" True )
   ,
-  (23 , Job 23 22 "Biopsy" False )
+  (23 , Job 23 19 "Ultrasound" False )
   ,
-  (24 , Job 24 1 "Cannula" False )
+  (24 , Job 24 28 "Ultrasound" False )
   ]
